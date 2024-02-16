11426504e38d89f5c14a116f619710ab20cd259d
Leon-Masakuna <masakunamfengleon@gmail.com>
2024-02-16 15:32:04 +0100

    fix : code update

--- app.py
+++ app.py
@@ -27,5 +27,4 @@ def profile(username):
     return f'Bienvenue sur votre page de profil, {username}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
